6c298989734659d4cb4de080704dfe57894f3fa9
Saurav Vishal <saurav.vishal@overdose.digital>
2023-08-18 18:27:20 +0530

    project init

---
+++ constants.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    entryPointUriPath: 'test1',
+    name: "graphql-commerce-tool",
+    description: "graphql-commerce-tool",
+    applicationId: "cllf6yq9l0004wh01sz7urtpw"
+};
---
+++ src/server.js
@@ -0,0 +1,66 @@
+// const express = require('express');
+// const { createServer } = require('http');
+
+// const { ApolloServer } = require('apollo-server-express');
+// const { typeDefs } = require('./graphql/schema');
+// const { resolvers } = require('./graphql/result');
+// const { PORT } = require("./config/config");
+// const cors = require('cors');
+
+
+// class App {
+//     async startServer() {
+//         try {
+//             const app = express();
+//             app.use(cors());
+//             const server = new ApolloServer({ 
+//                 typeDefs, 
+//                 resolvers,
+//                 introspection: true,
+//                 playground: true,
+//              });
+//             await server.start();
+//             server.applyMiddleware({ app, path: '/graphql' });
+
+//             const httpServer = createServer(app);
+
+//             httpServer.listen(PORT, () => {
+//                 console.log(`Server is running on port ${PORT}`);
+//             });
+//         } catch (error) {
+//             console.log("Internal Server Error", error);
+//         }
+//     }
+// }
+
+// module.exports = new App();
+
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
+const { typeDefs } = require('./graphql/schema');
+const resolvers = require('./graphql/result');
+const { PORT } = require("./config/config");
+const { default: expressPlayground } = require('graphql-playground-middleware-express');
+class App {
+    async startServer() {
+        try {
+            const app = express();
+            app.use("/graphql", graphqlHTTP((req) => ({
+                schema: typeDefs,
+                rootValue: resolvers,
+                graphiql: true
+            })));
+
+            app.get("/", expressPlayground({ endpoint: "/graphql" }));
+
+            app.listen({ port: PORT }, () => {
+                console.log(`Server running at :${PORT}`);
+            });
+        } catch (error) {
+            console.log("Internal Server Error", error);
+        }
+    }
+}
+
+module.exports = new App();
